d1417d36bd6d42c763b9c68bb2ccc9fb9ace199f
yeasinarafat1 <yachenarafath@gmail.com>
2024-01-21 12:53:08 +0600

    hi

--- script.js
+++ script.js
@@ -15,11 +15,34 @@ async function main(){
        }
     }
     let container = document.getElementById("container")
-    for(const img of imgs ){
-        // console.log(img);
-        container.innerHTML = container.innerHTML + `<img src="${img}" alt="">`
+    // for(const img of imgs ){
+    //     // console.log(img);
+    //     container.innerHTML = container.innerHTML + `<img src="${img}" alt="">`
         
-    }
+    // }
+imgs.forEach((element,index) => {
+    // console.log(index,element);
+    let title = element.split('/')[4]
+    container.innerHTML = container.innerHTML + `<div onclick="download(${index})" class="card">
+    <img id="${index}" src="${element}" alt="Card image">
+    <div class="card-overlay">
+      <div class="part-2">
+      <h2 class="card-title">${title}</h2>
+      <a id="download-logo" href="${element}" download class="download-logo">
+        <img src="download.svg" alt="Download">
+      </a>
+      </div>
+    </div>
+  </div>`
+    console.log(element.split('/')[4],index);
+  
+    
+    
+});
+
+    
+}
+
     
     // console.log(img);
     
@@ -28,5 +51,8 @@ async function main(){
     
     
     
-}
+    function download(id){
+      let src = document.getElementById(id)
+      console.log(src.src);
+    }
 main()
